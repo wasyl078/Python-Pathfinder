43f0c010000faf7a1b54132d291a57f62168274f
Student 246699 <246699@student.pwr.edu.pl>
2020-02-15 16:03:46 +0100

    setup, executable files

---
+++ executable_files/blocks/player_block.py
@@ -0,0 +1,50 @@
+# imports
+import pygame
+from blocks.abstract_block import AbtractBlock, abstractmethod
+from framework.consts_values import Blocks, Color
+
+
+# player is standard player character
+class Player(AbtractBlock):
+    # constructor - setting player object
+    def __init__(self, posx, posy):
+        super().__init__(posx, posy, Color.GREEN, Blocks.PLAYER)
+        print("player ")
+
+    # player's update is handling keyboard events
+    @abstractmethod
+    def update(self, matrix):
+        keys = pygame.key.get_pressed()
+        # input
+        if keys[pygame.K_RIGHT] and matrix.check(self.position_x + 1, self.position_y):
+            self.position_x += 1
+        if keys[pygame.K_LEFT] and matrix.check(self.position_x - 1, self.position_y):
+            self.position_x -= 1
+        if keys[pygame.K_DOWN] and matrix.check(self.position_x, self.position_y + 1):
+            self.position_y += 1
+        if keys[pygame.K_UP] and matrix.check(self.position_x, self.position_y - 1):
+            self.position_y -= 1
+
+    # another way to handle keyboard events by Player
+    def update_single_jump(self, matrix, event):
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            if matrix.check(self.position_x + 1, self.position_y):
+                self.position_x += 1
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            if matrix.check(self.position_x - 1, self.position_y):
+                self.position_x -= 1
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            if matrix.check(self.position_x, self.position_y + 1):
+                self.position_y += 1
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            if matrix.check(self.position_x, self.position_y - 1):
+                self.position_y -= 1
+
+    @abstractmethod
+    def render(self, screen):
+        super().render(screen)
+
+    @abstractmethod
+    def __bool__(self):
+        return False
---
+++ setup.py
@@ -0,0 +1,14 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {"packages": ["pygame"], "include_files": ["blocks/", "framework/", "graphs/"]}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(name="Pathifinder",
+      version="1.0",
+      description="My GUI application!",
+      options={"build_exe": build_exe_options},
+      executables=[Executable("pathfinder_v1.py", base=base)])
