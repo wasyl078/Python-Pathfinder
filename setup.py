2fb3e5b66fc142832255b9765e5ba12870490a0d
Student 246699 <246699@student.pwr.edu.pl>
2020-02-15 18:15:10 +0100

    no exe

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from cx_Freeze import setup, Executable
 build_exe_options = {"packages": ["pygame"], "include_files": ["blocks/", "framework/", "graphs/"]}
 
 base = None
-
+if sys.platform == "win32":
+    base = "win32GUI"
 
 setup(name="Pathifinder",
       version="1.4",
       description="My GUI application!",
       options={"build_exe": build_exe_options},
-      executables=[Executable("pathfinder_v1.py", base=base)])
-
+      executables=[Executable("pathfinder.py", base=base)])
