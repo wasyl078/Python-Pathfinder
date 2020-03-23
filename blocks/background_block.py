57863356450a226144c4e61c52c3d46cdc5e1bf8
Wasyl078 <wasilewski078@gmail.com>
2020-03-23 22:04:51 +0100

    cx_Freeze, dir build and new .exe inside build update

--- blocks/background_block.py
+++ blocks/background_block.py
@@ -3,7 +3,6 @@ from abc import abstractmethod
 from blocks.abstract_block import AbtractBlock
 from general.consts_values import Color, Blocks
 
-
 # background - block in which moveable objects can move into without any problems
 class Background(AbtractBlock):
     def __init__(self, pos_x: int, pos_y: int):
---
+++ setup.py
@@ -0,0 +1,16 @@
+from cx_Freeze import setup, Executable
+
+includefiles = ['blocks/', 'boards/', 'general/', 'graphs/', 'pictures/']
+includes = []
+excludes = []
+packages = ['pygame']
+
+setup(
+    name='Python Pathfinder',
+    version='0.1',
+    description='Two player bomberman like game',
+    author='Wasyl',
+    author_email='wasilewski078@gmail.com',
+    options={'build_exe': {'includes': includes, 'excludes': excludes, 'packages': packages, 'include_files': includefiles}},
+    executables=[Executable('pathfinder.py')]
+)
